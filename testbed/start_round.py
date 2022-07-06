e75e8348c0e619b6e26301873fcaf77c2d59519d
Henrique Dias <hacdias@gmail.com>
2022-07-06 13:27:02 +0200

    feat: use click for start_round

--- testbed/start_round.py
+++ testbed/start_round.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import argparse
+import click
 import logging
 import time
 import random
@@ -33,35 +33,29 @@ def get_owner_account(data_dir):
   account_password = accounts['owner'][account_address]
   return account_address, account_password
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data-dir', type=str, help='ethereum data directory path', default=utilities.default_datadir)
-  parser.add_argument('--provider', type=str, help='ethereum API provider', default='http://127.0.0.1:8545')
-  parser.add_argument('--rounds', type=int, help='how many rounds to run', default=1)
-  parser.add_argument('--contract', type=str, help='contract address', required=True)
-  parser.add_argument('--abi', type=str, help='abi path', default='../build/contracts/NoScoring.json')
-  parser.add_argument('--trainers', type=str, help='trainers selection mode', default='random')
-  parser.add_argument('--aggregators', type=str, help='aggregators selection mod', default='all')
-  parser.add_argument('--scoring', type=str, help='scoring mechanism', default='none')
-  parser.add_argument('--val', type=str, help='validation data', default='./datasets/mnist/5/owner_val.npz')
-  args = parser.parse_args()
-
-  account_address, account_password = get_owner_account(args.data_dir)
-
-  contract = blocklearning.Contract(log, args.provider, args.abi, account_address, account_password, args.contract)
+@click.command()
+@click.option('--provider', default='http://127.0.0.1:8545', help='web3 API HTTP provider')
+@click.option('--abi', default='../build/contracts/NoScoring.json', help='contract abi file')
+@click.option('--contract', required=True, help='contract address')
+@click.option('--scoring', default='none', help='scoring method')
+@click.option('--trainers', default='random', help='clients selection method')
+@click.option('--aggregators', default='all', help='servers selection method')
+@click.option('--data-dir', default=utilities.default_datadir, help='ethereum data directory path')
+@click.option('--val', default='./datasets/mnist/5/owner_val.npz', help='validation data .npz file')
+@click.option('--rounds', default=1, type=click.INT, help='number of rounds')
+def main(provider, abi, contract, scoring, trainers, aggregators,  data_dir, val, rounds):
+  account_address, account_password = get_owner_account(data_dir)
+  contract = blocklearning.Contract(log, provider, abi, account_address, account_password, contract)
   weights_loader = weights_loaders.IpfsWeightsLoader()
   model_loader = model_loaders.IpfsModelLoader(contract, weights_loader)
   model = model_loader.load()
-
-  weights_count = len(model.get_weights())
-  x_val, y_val = butilities.numpy_load(args.val)
+  x_val, y_val = butilities.numpy_load(val)
 
   aggregator_index = -1
 
   def choose_participants():
-    trainers = contract.get_trainers()
-    aggregators = contract.get_aggregators()
-    scorers = [] if args.scoring == 'none' else contract.get_scorers()
+    all_trainers = contract.get_trainers()
+    all_aggregators = contract.get_aggregators()
 
     global aggregator_index
 
@@ -69,23 +63,23 @@ if __name__ == '__main__':
     round_aggregators = None
     round_scorers = None
 
-    if args.trainers == 'random':
-      n = random.randint(len(trainers) // 2, len(trainers))
-      round_trainers = random.sample(trainers, n)
-    elif args.trainers == 'fcfs':
-      round_trainers = random.randint(len(trainers) // 2, len(trainers))
-    elif args.trainers == 'all':
-      round_trainers = trainers
+    if trainers == 'random':
+      n = random.randint(len(all_trainers) // 2, len(all_trainers))
+      round_trainers = random.sample(all_trainers, n)
+    elif trainers == 'fcfs':
+      round_trainers = random.randint(len(all_trainers) // 2, len(all_trainers))
+    elif trainers == 'all':
+      round_trainers = all_trainers
 
-    if args.aggregators == 'all':
-      round_aggregators = aggregators
-    elif args.aggregators == 'rr':
-      aggregator_index = (aggregator_index + 1) % len(aggregators)
-      round_aggregators = [aggregators[aggregator_index]]
+    if aggregators == 'all':
+      round_aggregators = all_aggregators
+    elif aggregators == 'rr':
+      aggregator_index = (aggregator_index + 1) % len(all_aggregators)
+      round_aggregators = [all_aggregators[aggregator_index]]
 
-    if args.scoring == 'multi-krum' or args.scoring=='data-validity':
+    if scoring == 'multi-krum' or scoring=='data-validity':
       round_scorers = round_aggregators
-    elif args.scoring == 'blockflow' or args.scoring == 'marginal-gain':
+    elif scoring == 'blockflow' or scoring == 'marginal-gain':
       round_scorers = round_trainers
 
     return round_trainers, round_aggregators, round_scorers
@@ -98,13 +92,13 @@ if __name__ == '__main__':
     log.info(json.dumps({ 'event': 'eval_end', 'ts': time.time_ns(), 'round': round }))
     return accuracy
 
-  for i in range(0, args.rounds):
+  for i in range(0, rounds):
     trainers, aggregators, scorers = choose_participants()
     log.info(json.dumps({ 'event': 'start', 'trainers': trainers, 'aggregators': aggregators, 'scorers': scorers, 'ts': time.time_ns() }))
 
-    if args.trainers == 'fcfs':
+    if trainers == 'fcfs':
       contract.start_quorum_round(trainers, aggregators)
-    elif args.scoring != 'none':
+    elif scoring != 'none':
       contract.start_round_with_scoring(trainers, aggregators, scorers)
     else:
       contract.start_round(trainers, aggregators)
@@ -119,3 +113,5 @@ if __name__ == '__main__':
     accuracy = eval_model(weights)
 
     log.info(json.dumps({ 'event': 'end', 'ts': time.time_ns(), 'round': round, 'weights': weights, 'accuracy': butilities.float_to_int(accuracy) }))
+
+main()
